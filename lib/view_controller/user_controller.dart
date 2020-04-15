a218efe9473a8da1d02e33d0906b1024faa6a1c5
myracledesign <weber.max.1988@live.de>
2020-04-15 09:48:09 +0200

    Added the init User to UserController

--- lib/view_controller/user_controller.dart
+++ lib/view_controller/user_controller.dart
@@ -1,7 +1,20 @@
+import 'package:firebaseprofiletutorial/locator.dart';
 import 'package:firebaseprofiletutorial/models/user_model.dart';
+import 'package:firebaseprofiletutorial/repository/auth_repo.dart';
 
 class UserController {
   UserModel _currentUser;
+  AuthRepo _authRepo = locator.get<AuthRepo>();
+  Future init;
+
+  UserController() {
+    init = initUser();
+  }
+
+  Future<UserModel> initUser() async {
+    _currentUser = await _authRepo.getUser();
+    return _currentUser;
+  }
 
   UserModel get currentUser => _currentUser;
 }
